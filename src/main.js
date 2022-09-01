f5c44a60b413a15664724fbb4da20a229b42b66a
Ivica <ivica-matic@outlook.com>
2022-09-01 15:14:53 +0100

    Implemented utility that checks bounding boxes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-
-const express = require('express');
-const express_queue = require('express-queue');
+const express = require("express");
+const express_queue = require("express-queue");
+const validateBbox = require("./validateBbox.js");
 
 const app = express();
+app.use(express.json());
 
 app.use(express_queue({ activeLimit: 1, queuedLimit: -1 }));
 
-app.post('/ingest', async (req, res) => {
-    res.send('Hello World!');
+app.post("/ingest", async (req, res) => {
+  let bbox = req.body.bbox;
+  if (bbox) {
+    if (!validateBbox(bbox)) {
+      res.status(400).send("Invalid bbox");
+      return;
+    }
+  }
+  res.status(200).send("OK");
 });
 
-app.listen(9000,"0.0.0.0");
+app.listen(9000, "0.0.0.0");
