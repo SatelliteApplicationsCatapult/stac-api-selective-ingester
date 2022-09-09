cb6660b2d9f7f97c5ce0c1d8724ad137c046d4ac
Ivica <ivica-matic@outlook.com>
2022-09-09 14:27:37 +0100

    Added filtering to exclude empty params if they are sent

--- src/main.js
+++ src/main.js
@@ -3,30 +3,30 @@ const express_queue = require("express-queue");
 const ingester = require("./StacSelectiveIngester.js");
 const app = express();
 const fs = require("fs");
+const { type } = require("os");
 app.use(express.json());
-app.use(express_queue({ activeLimit: 1, queuedLimit: -1 }));
 
 // declare a js queue
 let _executionQueue = [];
 
-app.post("/ingest", async (req, res) => {
+app.post("/ingest", express_queue({ activeLimit: 1, queuedLimit: -1 }),async (req, res) => {
   let body = req.body;
-  let sourceStacApiUrl = body.source_stac_api_url;
+  let sourceStacApiUrl = body.source_stac_catalog_url;
   if (!sourceStacApiUrl) {
-    return res.status(400).send("source_stac_api_url not found in body");
+    return res.status(400).send("source_stac_catalog_url not found in body");
   }
   // if sourceStacApiUrl ends with a slash, remove it
   if (sourceStacApiUrl.endsWith("/")) {
     sourceStacApiUrl = sourceStacApiUrl.slice(0, -1);
   }
 
-  delete body.source_stac_api_url;
+  delete body.source_stac_catalog_url;
   console.log("Source stac api url: ", sourceStacApiUrl);
-  let targetStacApiUrl = body.target_stac_api_url;
+  let targetStacApiUrl = body.target_stac_catalog_url;
   if (!targetStacApiUrl) {
-    return res.status(400).send("target_stac_api_url not found in body");
+    return res.status(400).send("target_stac_catalog_url not found in body");
   }
-  delete body.target_stac_api_url;
+  delete body.target_stac_catalog_url;
   console.log("Target stac api url: ", targetStacApiUrl);
   // if targetStacApiUrl ends with a slash, remove it
   if (targetStacApiUrl.endsWith("/")) {
@@ -46,6 +46,24 @@ app.post("/ingest", async (req, res) => {
   for (let key in body) {
     //console.log("key: ", key);
     let value = body[key];
+    // if value is null or undefined, skip it
+    if (!value) {
+      continue;
+    }
+    if(typeof(value) === "object"){
+      // if it is array
+      if(Array.isArray(value)){
+        // if it is empty array, skip it
+        if(value.length === 0){
+          continue;
+        }
+      } else {
+        // if it is empty object, skip it
+        if(Object.keys(value).length === 0){
+          continue;
+        }
+      }
+    }
     if (key === "bbox" || key === "collections" || key === "ids") {
       // convert value list to string delimited by commas
       value = value.join(",");
@@ -65,6 +83,7 @@ app.post("/ingest", async (req, res) => {
     _executionQueue.push(stacSelectiveIngester);
     let executionQueueLength = _executionQueue.length;
     let operationsPendingBeforeThisOne = executionQueueLength - 1;
+    console.log("Length of execution queue: ", executionQueueLength);
     return res
       .status(200)
       .send(
